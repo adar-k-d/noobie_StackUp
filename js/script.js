045fad64dd3ce4b1b42a5d0c5e911ea0dced8594
oldstaar123 <ashwinmenon2003@gmail.com>
2023-11-09 20:25:10 +0530

    changed logo

--- js/script.js
+++ js/script.js
@@ -30,4 +30,49 @@ function autoScroll() {
     }
 }
 
-let scrollInterval = setInterval(autoScroll, 3000); // Adjust this value to change the delay between scrolls
+let scrollInterval = setInterval(autoScroll, 1000); // Adjust this value to change the delay between scrolls
+
+
+
+let signupBtn=document.getElementById("signupBtn");
+let signinBtn=document.getElementById("signinBtn");
+let nameField=document.getElementById("nameField");
+let title=document.getElementById("title");
+
+signinBtn.onclick = function(){
+    nameField.style.maxHeight = "0";
+    title.innerHTML="Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+}
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = "60px";
+    title.innerHTML="Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+}
+
+const loginform = document.getElementById('loginform');
+        const loginbutton = document.getElementById('loginbutton');
+
+        // Add a click event listener to the "Login" button
+        loginbutton.addEventListener('click', function() {
+            // Toggle the visibility of the login form
+           
+            if (loginform.style.display === 'none' || loginform.style.display === '') {
+                loginform.style.display = 'block';
+            } else {
+                loginform.style.display = 'none';
+            }
+        });
+      
+
+const closeButton = document.getElementById("closeButton");
+const loginForm = document.getElementById("loginform");
+
+if (closeButton && loginForm) {
+    closeButton.addEventListener("click", function () {
+        loginForm.style.display = "none";
+    });
+}
+        
